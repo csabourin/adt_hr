2ba909213420bd1844fb6cb15f29735ad7ec39e8
Segalex <96079629+Segalex@users.noreply.github.com>
2021-12-16 09:57:53 -0500

    English audio timings have been added + started working on second chapter links

--- components/report_links.vue
+++ components/report_links.vue
@@ -3,11 +3,46 @@
     <div v-if="$i18n.locale=='fr'">
       <div>
         <h4>Valeurs et normes fondamentales</h4>
-        <ul>
-          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-fra.aspx?id=31300">Politique sur les résultats</externalLink></li>
-          <li><externalLink link="https://www.tbs-sct.gc.ca/ems-sgd/edb-bdd/index-fra.html#start">InfoBase du GC</externalLink> (Trouver l’information la plus récente sur les finances, l’effectif et les résultats du gouvernement)</li>
-          <li><externalLink link="https://www.canada.ca/fr/secretariat-conseil-tresor/services/etablissement-rapports-depenses/rapports-financiers-trimestriels.html">Rapports financiers trimestriels</externalLink></li>
-          <li><externalLink link="https://www.canada.ca/fr/secretariat-conseil-tresor/sujets/verifications-evaluations.html">Audit interne et évaluation</externalLink></li>
+        <ul class="list-unstyled">
+          <li><externalLink link="https://www.canada.ca/fr/commission-fonction-publique/services/activites-politiques.html">Activités politiques</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/burolis/search-recherche/search-recherche-eng.aspx">Activités politiques – Candidature</externalLink></li>
+          <li><externalLink link="https://laws-lois.justice.gc.ca/eng/acts/H-6/index.html/">Activités politiques – Prenez une décision éclairée</externalLink></li>
+          <li><externalLink link="https://laws-lois.justice.gc.ca/eng/const/page-1.html">Base de données Burolis</externalLink></li>
+          <li><externalLink link="https://laws-lois.justice.gc.ca/eng/Const/page-12.html">Ce que ne sont pas le harcèlement et la violence</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-fra.aspx?id=25049">Code de valeurs et d’éthique du secteur public</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=32627">Collaborer pour atteindre des résultats pour les Canadiennes et les Canadiens</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=26164">Conseil national mixte</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=26168">Déclaration des Nations Unies sur les Droits des Peuples Autochtones</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=27146">Directive sur l’application du Règlement sur les langues officielles – communications avec le public et prestation des services</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=26163">Directive sur la gestion du rendement</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=32671">Directive on the Prevention and Resolution of Workplace Harassment and Violence</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/employment-social-development/programs/workplace-health-safety/harassment-violence-prevention.html#Examples_of_harassment">Examples of harassment and violence</externalLink></li>
+          <li><externalLink link="https://laws-lois.justice.gc.ca/eng/acts/p-33.3/FullText.html">Federal Public Sector Labour Relations Act</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=22370">Guidelines for Discipline</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=27987">Guidelines for Essential Services Designations</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=12602">Guidelines on Managerial or Confidential Exclusions</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/government/publicservice/wellness-inclusion-diversity-public-service/harassment-conflict-resolution/informal-conflict-management-system/guide-key-elements-icms-core-public-administration.html">Guide to the key elements of an ICMS in the core public administration</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/public-service-commission/services/political-activities/i-want-to-become-a-candidate.html">I want to become a candidate</externalLink></li>
+          <li><externalLink link="https://www.clo-ocol.gc.ca/html/stu_etu_032011_p7_e.php">Leadership Competencies Profile for Official Languages</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/treasury-board-secretariat/services/values-ethics/official-languages/list-bilingual-regions-canada-language-of-work-purposes.html">List of Bilingual Regions of Canada for Language-of-Work Purposes</externalLink></li>
+          <li><externalLink link="https://www.njc-cnm.gc.ca/en">National Joint Council</externalLink></li>
+          <li><externalLink link="https://www.njc-cnm.gc.ca/directive/d7/v23/s252/en%22 /l %22s252-tc-tm_3">Occupational Health and Safety Directive</externalLink></li>
+          <li><externalLink link="https://laws-lois.justice.gc.ca/eng/acts/o-3.01/">Official Languages Act</externalLink></li>
+          <li><externalLink link="https://laws.justice.gc.ca/eng/regulations/sor-92-48/index.html">Official Languages (Communications with and Services to the Public) Regulations</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=26160">Policy on Official Languages</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/(S(e021jvamqbhco5551teiyz55))/doc-eng.aspx?id=32621">Policy on People Management</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/public-service-commission/services/political-activities.html">Political Activities</externalLink></li>
+          <li><externalLink link="https://learn-apprendre.csps-efpc.gc.ca/application/en/search/site/political activities">Political Activities: Candidacy </externalLink></li>
+          <li><externalLink link="https://learn-apprendre.csps-efpc.gc.ca/application/en/search/site/political activities">Political Activities: Making an Informed Decision</externalLink></li>
+          <li><externalLink link="https://laws-lois.justice.gc.ca/eng/acts/p-31.9/">Public Servants Disclosure Protection Act</externalLink></li>
+          <li><externalLink link="http://gazette.gc.ca/rp-pr/p2/2020/2020-06-24/html/sor-dors130-eng.html">Work Place Harassment and Violence Prevention Regulations</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/employment-social-development/programs/workplace-health-safety/harassment-violence-prevention.html">Requirements for employers to prevent harassment and violence in federally regulated workplaces</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/employment-social-development/programs/workplace-health-safety/harassment-violence-prevention/complaints/resolve.html">Resolve a workplace harassment and violence complaint</externalLink></li>
+          <li><externalLink link="https://sencanada.ca/content/sen/committee/432/CIBA/reports/CIBA_RTP5_POL_HVP_E.pdf">Senate Harassment and Violence Prevention Policy</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/treasury-board-secretariat/topics/audit-evaluation.html">Suite of tools related to other political activities</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=25049">Values and Ethics Code for the Public Sector</externalLink></li>
+          <li><externalLink link="https://www.un.org/development/desa/indigenouspeoples/declaration-on-the-rights-of-indigenous-peoples.html">United Nations Declaration on the Rights of Indigenous Peoples</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/employment-social-development/programs/workplace-health-safety/harassment-violence-prevention.html#What_is_not">What is not harassment and violence</externalLink></li>
         </ul>
       </div>
     </div>
@@ -15,10 +50,45 @@
       <div>
         <h4>Core Values and Standards</h4>
         <ul class="list-unstyled">
-          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=31300">Policy on Results</externalLink></li>
-          <li><externalLink link="https://www.tbs-sct.gc.ca/ems-sgd/edb-bdd/index-eng.html#start">GC InfoBase</externalLink> (Find the latest information on all government finances, people and results)</li>
-          <li><externalLink link="https://www.canada.ca/en/treasury-board-secretariat/services/reporting-government-spending/quarterly-financial-reporting.html">Quarterly Financial Report</externalLink></li>
-          <li><externalLink link="https://www.canada.ca/en/treasury-board-secretariat/topics/audit-evaluation.html">Internal Audit and Evaluation</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/public-service-commission/services/appointment-framework/guides-tools-appointment-framework/assessment-official-languages-appointment-process.html">Assessment of official languages in the appointment process</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/burolis/search-recherche/search-recherche-eng.aspx">Burolis database</externalLink></li>
+          <li><externalLink link="https://laws-lois.justice.gc.ca/eng/acts/H-6/index.html/">Canadian Human Rights Act</externalLink></li>
+          <li><externalLink link="https://laws-lois.justice.gc.ca/eng/const/page-1.html">Constitution Act, 1867</externalLink></li>
+          <li><externalLink link="https://laws-lois.justice.gc.ca/eng/Const/page-12.html">Constitution Act, 1982, Part 1: Canadian Charter of Rights and Freedoms</externalLink></li>
+          <li><externalLink link="https://learn-apprendre.csps-efpc.gc.ca/application/en/content/collaborating-deliver-results-canadians">Collaborating to Deliver Results to Canadians</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=32627">Directive on Conflict of Interest </externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=26164">Directive on Official Languages for Communications and Services</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=26168">Directive on Official Languages for People Management </externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=27146">Directive on Performance Management</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=26163">Directive on the Implementation of the Official Languages (Communications with and Services to the Public) Regulations</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=32671">Directive on the Prevention and Resolution of Workplace Harassment and Violence</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/employment-social-development/programs/workplace-health-safety/harassment-violence-prevention.html#Examples_of_harassment">Examples of harassment and violence</externalLink></li>
+          <li><externalLink link="https://laws-lois.justice.gc.ca/eng/acts/p-33.3/FullText.html">Federal Public Sector Labour Relations Act</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=22370">Guidelines for Discipline</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=27987">Guidelines for Essential Services Designations</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=12602">Guidelines on Managerial or Confidential Exclusions</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/government/publicservice/wellness-inclusion-diversity-public-service/harassment-conflict-resolution/informal-conflict-management-system/guide-key-elements-icms-core-public-administration.html">Guide to the key elements of an ICMS in the core public administration</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/public-service-commission/services/political-activities/i-want-to-become-a-candidate.html">I want to become a candidate</externalLink></li>
+          <li><externalLink link="https://www.clo-ocol.gc.ca/html/stu_etu_032011_p7_e.php">Leadership Competencies Profile for Official Languages</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/treasury-board-secretariat/services/values-ethics/official-languages/list-bilingual-regions-canada-language-of-work-purposes.html">List of Bilingual Regions of Canada for Language-of-Work Purposes</externalLink></li>
+          <li><externalLink link="https://www.njc-cnm.gc.ca/en">National Joint Council</externalLink></li>
+          <li><externalLink link="https://www.njc-cnm.gc.ca/directive/d7/v23/s252/en%22 /l %22s252-tc-tm_3">Occupational Health and Safety Directive</externalLink></li>
+          <li><externalLink link="https://laws-lois.justice.gc.ca/eng/acts/o-3.01/">Official Languages Act</externalLink></li>
+          <li><externalLink link="https://laws.justice.gc.ca/eng/regulations/sor-92-48/index.html">Official Languages (Communications with and Services to the Public) Regulations</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=26160">Policy on Official Languages</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/(S(e021jvamqbhco5551teiyz55))/doc-eng.aspx?id=32621">Policy on People Management</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/public-service-commission/services/political-activities.html">Political Activities</externalLink></li>
+          <li><externalLink link="https://learn-apprendre.csps-efpc.gc.ca/application/en/search/site/political activities">Political Activities: Candidacy </externalLink></li>
+          <li><externalLink link="https://learn-apprendre.csps-efpc.gc.ca/application/en/search/site/political activities">Political Activities: Making an Informed Decision</externalLink></li>
+          <li><externalLink link="https://laws-lois.justice.gc.ca/eng/acts/p-31.9/">Public Servants Disclosure Protection Act</externalLink></li>
+          <li><externalLink link="http://gazette.gc.ca/rp-pr/p2/2020/2020-06-24/html/sor-dors130-eng.html">Work Place Harassment and Violence Prevention Regulations</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/employment-social-development/programs/workplace-health-safety/harassment-violence-prevention.html">Requirements for employers to prevent harassment and violence in federally regulated workplaces</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/employment-social-development/programs/workplace-health-safety/harassment-violence-prevention/complaints/resolve.html">Resolve a workplace harassment and violence complaint</externalLink></li>
+          <li><externalLink link="https://sencanada.ca/content/sen/committee/432/CIBA/reports/CIBA_RTP5_POL_HVP_E.pdf">Senate Harassment and Violence Prevention Policy</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/treasury-board-secretariat/topics/audit-evaluation.html">Suite of tools related to other political activities</externalLink></li>
+          <li><externalLink link="https://www.tbs-sct.gc.ca/pol/doc-eng.aspx?id=25049">Values and Ethics Code for the Public Sector</externalLink></li>
+          <li><externalLink link="https://www.un.org/development/desa/indigenouspeoples/declaration-on-the-rights-of-indigenous-peoples.html">United Nations Declaration on the Rights of Indigenous Peoples</externalLink></li>
+          <li><externalLink link="https://www.canada.ca/en/employment-social-development/programs/workplace-health-safety/harassment-violence-prevention.html#What_is_not">What is not harassment and violence</externalLink></li>
         </ul>
       </div>
     </div>
